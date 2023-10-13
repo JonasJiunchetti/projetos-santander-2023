f0dbd1794e18a269968c566bba4fe2a299ce5d78
Jonas J <jonas.jiunchetti@outlook.com>
2023-10-12 21:40:29 -0300

    Cliente, Endereco, ORM, H2 database configuration

---
+++ projetos/lab-projeto-spring/src/main/java/one/digitalinnovation/spring/entities/Cliente.java
@@ -0,0 +1,23 @@
+package one.digitalinnovation.spring.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class Cliente {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    private Endereco endereco;
+
+}
